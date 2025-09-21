e7ab0c726235bec92533625c4f0ccd1995364f56
Md. Abdullah Bin Sorwar Chowdhury <mdabdullahbins0009@gmail.com>
2025-09-21 12:50:12 +0600

    6th problem done

--- Problem6_Banking/BankAccount.java
+++ Problem6_Banking/BankAccount.java
@@ -7,22 +7,47 @@ public class BankAccount {
 
     public BankAccount(int accountNumber, String holderName, double openingBalance){
         // TODO: initialize fields, increment totalAccounts
-        throw new UnsupportedOperationException("TODO");
+        this.accountNumber = accountNumber;
+        this.holderName = holderName;
+        this.balance = openingBalance;
+        totalAccounts++;
+        // throw new UnsupportedOperationException("TODO");
     }
 
     public void deposit(double amount){
         // TODO: positive only
-        throw new UnsupportedOperationException("TODO");
+        if (amount > 0) {
+            balance += amount;
+            return;
+        }
+        else {
+            throw new IllegalArgumentException("Deposit amount must be positive");
+        }
+        // throw new UnsupportedOperationException("TODO");
     }
 
     public boolean withdraw(double amount){
         // TODO: positive and <= balance
-        throw new UnsupportedOperationException("TODO");
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            return true;
+        }
+        else {
+            return false;
+        }
+        // throw new UnsupportedOperationException("TODO");
     }
 
     public boolean transferTo(BankAccount other, double amount){
         // TODO: withdraw from this and deposit to other atomically
-        throw new UnsupportedOperationException("TODO");
+        if (this.withdraw(amount)) {
+            other.deposit(amount);
+            return true;
+        }
+        else {
+            return false;
+        }
+        // throw new UnsupportedOperationException("TODO");
     }
 
     public static int getTotalAccounts(){ return totalAccounts; }
