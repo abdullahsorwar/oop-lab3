6ab5c5af7f9d19432c1c9e45dedee8ec08ce19f1
Md. Abdullah Bin Sorwar Chowdhury <mdabdullahbins0009@gmail.com>
2025-09-21 12:24:42 +0600

    3rd problem done

--- Problem3_ShoppingCart/ShoppingCart.java
+++ Problem3_ShoppingCart/ShoppingCart.java
@@ -6,22 +6,41 @@ public class ShoppingCart {
 
     public void addProduct(Product p){
         // TODO: add product to list
-        throw new UnsupportedOperationException("TODO");
+        items.add(p);
+        // throw new UnsupportedOperationException("TODO");
     }
 
     public boolean removeProductById(int id){
         // TODO: remove first product with id; return true if removed
-        throw new UnsupportedOperationException("TODO");
+        for (int i = 0; i < items.size(); i++) {
+            if (items.get(i).getId() == id) {
+                items.remove(i);
+                return true;
+            }
+        }
+        return false;
+        // throw new UnsupportedOperationException("TODO");
     }
 
     public void applyDiscount(double percent){
         // TODO: set discountPercent (0..90 inclusive), else throw IllegalArgumentException
-        throw new UnsupportedOperationException("TODO");
+        if (percent < 0 || percent > 90) {
+            throw new IllegalArgumentException("Discount percent must be between 0 and 90 inclusive.");
+        }
+        this.discountPercent = percent;
+        return;
+        // throw new UnsupportedOperationException("TODO");
     }
 
     public double getTotalCost(){
         // TODO: sum of prices minus percent discount
-        throw new UnsupportedOperationException("TODO");
+        double total = 0.0;
+        for (Product p : items) {
+            total += p.getPrice();
+        }
+        total = total * (1 - discountPercent / 100);
+        return total;
+        // throw new UnsupportedOperationException("TODO");
     }
 
     public int size(){ return items.size(); }
