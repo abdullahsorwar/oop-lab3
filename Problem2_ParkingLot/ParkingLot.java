733fa17b486c3b5b941b047c96b32cb995617648
Md. Abdullah Bin Sorwar Chowdhury <mdabdullahbins0009@gmail.com>
2025-09-21 12:09:17 +0600

    2nd problem done

--- Problem2_ParkingLot/ParkingLot.java
+++ Problem2_ParkingLot/ParkingLot.java
@@ -6,14 +6,31 @@ public class ParkingLot {
 
     public boolean park(Vehicle v){
         // TODO: if already parked, return false; else add and return true
-        throw new UnsupportedOperationException("TODO");
+        if (parked.containsKey(v.getPlate())) {
+            return false;
+        } else {
+            parked.put(v.getPlate(), v);
+            return true;
+        }
+        // throw new UnsupportedOperationException("TODO");
     }
 
     public double leave(String plate, long exitTime){
         // TODO: compute hours (ceil to next hour if any fraction), fee = hours * rate
         // Remove from map, add to revenue, return fee
         // If not found, return -1
-        throw new UnsupportedOperationException("TODO");
+        if (!parked.containsKey(plate)) {
+            return -1;
+        }
+        else {
+            Vehicle v = parked.remove(plate);
+            long duration = exitTime - v.getEntryTime();
+            int hours = (int) Math.ceil(duration / 3600.0);
+            double fee = hours * v.ratePerHour();
+            revenue += fee;
+            return fee;
+        }
+        // throw new UnsupportedOperationException("TODO");
     }
 
     public double getRevenue(){ return revenue; }
