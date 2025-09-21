c6126b17bef4d5daaadc1c468e0dee288145ab60
Md. Abdullah Bin Sorwar Chowdhury <mdabdullahbins0009@gmail.com>
2025-09-21 14:18:10 +0600

    7th problem done

--- Problem7_Zoo/Zoo.java
+++ Problem7_Zoo/Zoo.java
@@ -8,12 +8,24 @@ public class Zoo {
     public void feedAll(){
         // TODO: For each animal, print: "<name> eats. Sound: <sound>"
         // Return type is void; we just require the exact print format for testing via a capture (not used here).
-        throw new UnsupportedOperationException("TODO");
+        for (Animal a : animals) {
+            System.out.println(a.getName() + " eats. Sound: " + a.makeSound());
+        }
+        // throw new UnsupportedOperationException("TODO");
     }
 
     public Animal oldest(){
+        int maxAge = -1;
+        Animal oldestAnimal = null;
+        for (Animal a : animals) {
+            if (a.getAge() > maxAge) {
+                maxAge = a.getAge();
+                oldestAnimal = a;
+            }
+        }
+        return oldestAnimal;
         // TODO: return oldest animal (null if none)
-        throw new UnsupportedOperationException("TODO");
+        // throw new UnsupportedOperationException("TODO");
     }
 
     public int size(){ return animals.size(); }
